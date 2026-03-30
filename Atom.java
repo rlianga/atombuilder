8bbc63ed31b09443615987466ecbb7b259cfdb00
rlianga <ryanlianga@gmail.com>
2026-03-29 17:55:31 -0700

    Add files via upload

--- Atom.java
+++ Atom.java
@@ -1,18 +1,18 @@
 package atombuilder;
 
 // !!! this class would only be used for a database to be searched through. all final values then no? no need to account for strange user inputs!!!
-public class Atom { //atomic number is index. Ngl prob gonna put some bs in index 0
-	private String name;
-	private int protons; 
-	private int neutrons; 
-	private int electrons; //procedural, but still here for simplicity
-	private int group; //if 0:     series lanthanoid = output group Lanthanide    >>    actinide = Actinoid
-	private int period;
-	private atomSeries series;
-	private int casNumber; //exclude the dashes, they will be re-added in output.wWhen searching for cas number, will accept either with dashes or without them (but no inbetween)
-	private double meltingPoint; //celsius. 0 means n/a or unknown
-	private double boilingPoint;
-	private String abbreviation; //procedural unless overridden
+public class Atom {
+	String name;
+	int protons; 
+	int neutrons; 
+	int electrons; //procedural, but still here for simplicity
+	int group; //if 0:     series lanthanoid = output group Lanthanide    >>    actinide = Actinoid
+	int period;
+	atomSeries series;
+	int casNumber; //exclude the dashes, they will be re-added in output.wWhen searching for cas number, will accept either with dashes or without them (but no inbetween)
+	double meltingPoint; //celsius. 0 means n/a or unknown
+	double boilingPoint;
+	String abbreviation; //procedural unless overridden
 
 	//constructor 
 	public Atom (String name, int protons, int neutrons, int group, int period, atomSeries series, int casNumber, double meltingPoint, double boilingPoint, String abbreviation) { 
@@ -34,41 +34,28 @@ public class Atom { //atomic number is index. Ngl prob gonna put some bs in inde
 		this(name, protons, neutrons, group, period, series, casNumber, meltingPoint, boilingPoint, name.substring(0,1));
 	}
 	
-	public static int getCharge(int protons, int electrons) {
-		return protons - electrons; //is this even necessary
-	}
-	
-	public static String getGroupStr(int group, atomSeries series) {
-		if (group == 0) {
-			if (series.equals(atomSeries.Lanthanoid)) {
+	public static String getGroup(Atom atom) {
+		if (atom.group == 0) {
+			if (atom.series.equals(atomSeries.Lanthanoid)) {
 				return "Lanthanide";
 			} else {
 				return "Actinide";
 			}
 		} else {
-			return Integer.toString(group);
+			return Integer.toString(atom.group);
 		}
 	}
 
-	public static double getWeight (int protons, int neutrons, int electrons) { //returns in AMU
-		return (protons * 1.00727) + (neutrons * 1.00866) + (electrons * 0.000549);
-	}
-
-	public static char getBlock (int group, int period) {
-		if (group == 0) { // 0 means lanthanide or actinide
+	public static char getBlock (Atom atom) {
+		if (atom.group == 0) { // 0 means lanthanide or actinide
 			return 'f';
-		} else if (3 <= group || group <= 12) {
+		} else if (3 <= atom.group || atom.group <= 12) {
 			return 'd';
-		} else if (group > 12 && period != 1) {
+		} else if (atom.group > 12 && atom.period != 1) {
 			return 'p';
 		} else {
 			return 's';
 		}
-	}
-
-	public static int getShellElectrons (int electrons, int shell) { //should 
-	//TODO should return # of electrons in corresponding shell. this will be implemented once we start on gui
-		return -1;
 	}
 
 }
--- atombuilder.java
+++ atombuilder.java
@@ -3,6 +3,24 @@ package atombuilder;
 import java.util.ArrayList;
 
 public class atombuilder {	
+	
+	public static int getShellElectrons(int electrons, int shell) { //should 
+	//TODO should return # of electrons in corresponding shell. this will be implemented once we start on gui
+		return -1;
+	}
+	
+	public static String atomNameStr(Atom atom) { //when saying atom name, say "atomname" + # of protons + neutrons (to account for isotopes), could just be infinitely scalable
+		return atom.name + "-" + (atom.protons + atom.neutrons);
+	}
+	
+	public static int getCharge(Atom atom) {
+		return atom.protons - atom.electrons; //is this even necessary
+	}
+	
+	public static double getWeight (Atom atom) { //returns in AMU
+		return (atom.protons * 1.00727) + (atom.neutrons * 1.00866) + (atom.electrons * 0.000549);
+	}
+	
 	public static void main (String[] args) {
 		ArrayList<Atom> periodic = new ArrayList<>(); //index = atomic number = # protons
 		
@@ -11,13 +29,24 @@ public class atombuilder {
 		periodic.add(new Atom("Hydrogen", 1, 0, 1, 1, atomSeries.Reactive_nonmetal, 1333740, -259.16, -252.879));
 		periodic.add(new Atom("Helium", 2, 2, 18, 1, atomSeries.Noble_gas, 7440597, -272.2, -268.928, "He"));
 		periodic.add(new Atom("Lithium", 3, 4, 1, 2, atomSeries.Alkali_metal, 7439932, 180.54, 1342, "Li"));
+		periodic.add(new Atom("Beryllium", 4, 5, 2, 2, atomSeries.Alkaline_earth_metal, 7440417, 1287, 2470, "Be"));
 		
 		//need to database everything else (to i assume #118 and then it'll just say "unknown".....
+		
+		
+		/*--------TESTING JUNK--------*/
+		Atom displayAtom = periodic.get(1);
+		System.out.println(atomNameStr(displayAtom));
+		displayAtom.neutrons += 5;
+		System.out.println(atomNameStr(displayAtom));
+		System.out.println(displayAtom.name + " has " + displayAtom.electrons + " electrons.");
+		
+		displayAtom = periodic.get(3);
+		System.out.println("Atom " + displayAtom.name + " is from group " + Atom.getGroup(displayAtom));
+		System.out.println(displayAtom.name + " is from the " + Atom.getBlock(displayAtom) + " block.");
 
 	}
 }
-
-//when saying atom name, say "atomname" + # of protons + neutrons (to account for isotopes), could just be infinitely scalable
 
 
 //obviously need to add safeguards for invalid inputs for all these functions, but the constructor should be fine as it’s only for databasing 
@@ -27,5 +56,5 @@ public class atombuilder {
 
 /* pretty much just trying to replicate this website 
 https://byjus.com/periodic-table/
-But with less data per atom and a rudimentary gui display + an atom creator 
+But with less data per atom and a rudimentary gui display + a procedural atom creator 
 */
