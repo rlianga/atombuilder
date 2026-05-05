d123dadc4773271b2d76d33c6b2fb21b646ef5c2
rlianga <ryanlianga@gmail.com>
2026-05-05 01:10:44 -0700

    refactoring methods to use instance variables

--- src/atombuilder/Atom.java
+++ src/atombuilder/Atom.java
@@ -27,37 +27,37 @@ public class Atom {
 		this.abbreviation = abbreviation;
 	}
 	
-	//without given abbreviation, procedurally makes it the first letter
+	//without given abbreviation, makes it the first letter
 	public Atom (String name, int protons, int neutrons, int group, int period, atomSeries series, int casNumber, double meltingPoint, double boilingPoint) {
 		this(name, protons, neutrons, group, period, series, casNumber, meltingPoint, boilingPoint, name.substring(0,1));
 	}
 	
-	public static String getGroup(Atom atom) {
-		if (atom.group == 0) {
-			if (atom.series.equals(atomSeries.Lanthanide)) {
+	public String getGroup() {
+		if (this.group == 0) {
+			if (this.series.equals(atomSeries.Lanthanide)) {
 				return "Lanthanide";
 			} else {
 				return "Actinide";
 			}
 		} else {
-			return Integer.toString(atom.group);
+			return Integer.toString(this.group);
 		}
 	}
 
-	public static char getBlock (Atom atom) {
-		if (atom.group == 0) {
+	public char getBlock () {
+		if (this.group == 0) {
 			return 'f';
-		} else if (3 <= atom.group || atom.group <= 12) {
+		} else if (3 <= this.group || this.group <= 12) {
 			return 'd';
-		} else if (atom.group > 12 && atom.period != 1) {
+		} else if (this.group > 12 && this.period != 1) {
 			return 'p';
 		} else {
 			return 's';
 		}
 	}
 	
-	public static String getSeriesString(atomSeries series) {
-		switch(series) {
+	public String getSeriesString() {
+		switch(this.series) {
 		case Reactive_nonmetal:
 			return "Reactive Nonmetal";
 		case Noble_gas:
@@ -79,6 +79,47 @@ public class Atom {
 		default:
 			return "Unknown";
 		}
+	}
+	
+	public String isotopeNameStr() {
+		return this.name + "-" + (this.protons + this.neutrons);
+	}
+	
+	public String casNumberStr() {
+		String returnStr = Integer.toString(this.casNumber);
+		int len = returnStr.length();
+		return returnStr.substring(0,len-3) + "-" + returnStr.substring(len-3,len-1) + "-" + returnStr.charAt(len-1);
+	}
+	
+	public int getCharge() {
+		return this.protons - this.electrons;
+	}
+	
+	public double getWeight () {
+		return (this.protons * 1.00727) + (this.neutrons * 1.00866) + (this.electrons * 0.000549); //in AMU
+	}
+	
+	public void updateElectrons(int electronCount) {
+		this.electrons += electronCount;
+	}
+	
+	public void updateProtons(int protonCount) {
+		this.protons += protonCount;
+	}
+	
+	public void updateNeutrons(int neutronCount) {
+		this.neutrons += neutronCount;
+	}
+	
+	public String getAtomInfoString() {
+		return this.abbreviation +
+				 "\nGroup: " + this.getGroup() + 
+				 "\nPeriod: " + this.period + 
+				 "\nBlock: " + this.getBlock() + " block" +
+				 "\nSeries: " + this.getSeriesString() + 
+				 "\nMelting Point: " + this.meltingPoint + 
+			 	 "\nBoiling Point: " + this.boilingPoint + 
+			 	 "\nCAS: " + this.casNumber;
 	}
 
 }
